e303a1de5c8753febe1f6f00de2b8c3be7e63225
OdirleiK <odirlei67777@gmail.com>
2024-02-26 20:10:56 -0300

    :sparkles: feat: add lombok

--- src/main/java/br/com/kmpx/quarkussocial/domain/model/User.java
+++ src/main/java/br/com/kmpx/quarkussocial/domain/model/User.java
@@ -1,16 +1,15 @@
 package br.com.kmpx.quarkussocial.domain.model;
 
-import java.util.Objects;
-
-import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Data;
 
 @Entity
 @Table(name="users")
+@Data
 public class User {
 
 	@Id
@@ -19,37 +18,5 @@ public class User {
 	private String name;
 	private Integer age;
 	
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public Integer getAge() {
-		return age;
-	}
-	public void setAge(Integer age) {
-		this.age = age;
-	}
-	@Override
-	public int hashCode() {
-		return Objects.hash(age, id, name);
-	}
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		User other = (User) obj;
-		return Objects.equals(age, other.age) && Objects.equals(id, other.id) && Objects.equals(name, other.name);
-	}
+
 }
