b8f59e2ee305d05703958d37503f3ffa94240fe0
OdirleiK <odirlei67777@gmail.com>
2024-03-02 15:52:03 -0300

    :test_tube: test: created createUserValidationErrorTest

--- src/test/java/br/com/kmpx/quarkussocial/rest/UserResourceTest.java
+++ src/test/java/br/com/kmpx/quarkussocial/rest/UserResourceTest.java
@@ -1,15 +1,20 @@
 package br.com.kmpx.quarkussocial.rest;
 
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.List;
+import java.util.Map;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import br.com.kmpx.quarkussocial.rest.dto.CreateUserResquest;
+import br.com.kmpx.quarkussocial.rest.dto.ResponseError;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
-
-import static io.restassured.RestAssured.given;
-import static org.junit.jupiter.api.Assertions.*;
 
 
 @QuarkusTest
@@ -33,6 +38,29 @@ class UserResourceTest {
 		
 		assertEquals(201, response.statusCode());
 		assertNotNull(response.jsonPath().getString("id"));
+	}
+	
+	@Test
+	@DisplayName("should return error when json is not valid")
+	public void createUserValidationErrorTest() {
+		CreateUserResquest user = new CreateUserResquest();
+		user.setName(null);
+		user.setAge(null);
+		
+		Response response = given()
+				.contentType(ContentType.JSON)
+				.body(user)
+			.when()
+				.post("/users")
+			.then()
+				.extract().response();
+		
+		assertEquals(ResponseError.UNPROCESSABLE_ENTITY_STATUS, response.statusCode());
+		assertEquals("Validation Error", response.jsonPath().getString("message"));
+		
+		List<Map<String, String>> errors = response.jsonPath().getList("errors");
+		assertNotNull(errors.get(0).get("message"));
+		assertNotNull(errors.get(1).get("message"));
 	}
 
 }
