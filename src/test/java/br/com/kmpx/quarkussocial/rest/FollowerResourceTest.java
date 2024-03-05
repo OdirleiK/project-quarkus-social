9e8869b58e8e3f002df9acb1867375e0912bad69
OdirleiK <odirlei67777@gmail.com>
2024-03-05 07:15:52 -0300

    :test_tube: test: created sameUserAsFollower test

---
+++ src/test/java/br/com/kmpx/quarkussocial/rest/FollowerResourceTest.java
@@ -0,0 +1,74 @@
+package br.com.kmpx.quarkussocial.rest;
+
+import static io.restassured.RestAssured.given;
+
+import javax.inject.Inject;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import br.com.kmpx.quarkussocial.domain.model.Follower;
+import br.com.kmpx.quarkussocial.domain.model.User;
+import br.com.kmpx.quarkussocial.domain.repository.FollowerRepository;
+import br.com.kmpx.quarkussocial.domain.repository.UserRepository;
+import br.com.kmpx.quarkussocial.rest.dto.FollowerRequest;
+import io.restassured.http.ContentType;
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.core.Response;
+
+
+class FollowerResourceTest {
+
+	@Inject
+    UserRepository userRepository;
+    @Inject
+    FollowerRepository followerRepository;
+
+    Long userId;
+    Long followerId;
+
+    @BeforeEach
+    @Transactional
+    void setUp() {
+        //usuario padrão dos testes
+        var user = new User();
+        user.setAge(30);
+        user.setName("Fulano");
+        userRepository.persist(user);
+        userId = user.getId();
+
+        // o seguidor
+        var follower = new User();
+        follower.setAge(31);
+        follower.setName("Cicrano");
+        userRepository.persist(follower);
+        followerId = follower.getId();
+
+        //cria um follower
+        var followerEntity = new Follower();
+        followerEntity.setFollower(follower);
+        followerEntity.setUser(user);
+        followerRepository.persist(followerEntity);
+    }
+    
+    @Test
+    @DisplayName("should return 409 when Follower Id is equal to User id")
+    public void sameUserAsFollowerTest(){
+
+        var body = new FollowerRequest();
+        body.setFollowerId(userId);
+
+        given()
+            .contentType(ContentType.JSON)
+            .body(body)
+            .pathParam("userId", userId)
+        .when()
+            .put()
+        .then()
+            .statusCode(Response.Status.CONFLICT.getStatusCode())
+            .body(Matchers.is("You can't follow yourself"));
+    }
+
+}
